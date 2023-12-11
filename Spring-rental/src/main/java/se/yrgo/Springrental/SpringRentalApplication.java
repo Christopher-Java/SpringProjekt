83b6e6122b3c2c56aa0a4258c0a156d4e655da9a
anton <antwil1107@skola.goteborg.se>
2023-12-11 10:50:48 +0100

    Added spring-rental, added gitignore

---
+++ Spring-rental/src/main/java/se/yrgo/Springrental/SpringRentalApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.Springrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRentalApplication.class, args);
+	}
+
+}
---
+++ Spring-rental/src/test/java/se/yrgo/Springrental/SpringRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.Springrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
