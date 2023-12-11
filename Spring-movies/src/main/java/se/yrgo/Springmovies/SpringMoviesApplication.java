c3d391242863062e4c73c43296abaffd76383819
anton <antwil1107@skola.goteborg.se>
2023-12-11 10:39:15 +0100

    added spring-movies

---
+++ Spring-movies/src/main/java/se/yrgo/Springmovies/SpringMoviesApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.Springmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMoviesApplication.class, args);
+	}
+
+}
---
+++ Spring-movies/src/test/java/se/yrgo/Springmovies/SpringMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.Springmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
