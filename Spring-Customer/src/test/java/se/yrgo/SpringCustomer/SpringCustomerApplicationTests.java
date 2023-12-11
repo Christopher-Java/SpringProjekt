8f34359d5984defbf812b38dbc4968e55c00c369
Christopher-Java <chrlom0905@skola.goteborg.se>
2023-12-11 10:54:13 +0100

    Adding Customer

---
+++ Spring-Customer/src/test/java/se/yrgo/SpringCustomer/SpringCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.SpringCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
