5d9343c30b27a3bc528374ce3678cee009774abc
Christopher-Java <chrlom0905@skola.goteborg.se>
2023-12-14 10:47:43 +0100

    Wrote methods and implemented them in RentalServiceImpl

--- Spring-rental/src/main/java/se/yrgo/domain/Rental.java
+++ Spring-rental/src/main/java/se/yrgo/domain/Rental.java
@@ -1,4 +1,60 @@
 package se.yrgo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
 public class Rental {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long customerId;
+    private Long movieId;
+    private int rentalCost;
+     private Date rentalDate;
+
+
+     //Setter and Getters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRentalCost() {
+        return rentalCost;
+    }
+
+    public void setRentalCost(int rentalCost) {
+        this.rentalCost = rentalCost;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
 }
