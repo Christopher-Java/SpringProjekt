370c3f216edef2401e96453227df36e8c04eb475
XiaoYanWidheLi <xiaoyanyan622@gmail.com>
2023-12-18 23:24:21 +0800

    add Customer constructor, getter and setter.

--- Spring-Customer/src/main/java/se/yrgo/domain/Customer.java
+++ Spring-Customer/src/main/java/se/yrgo/domain/Customer.java
@@ -14,5 +14,36 @@ public class Customer {
     private String name;
     private String email;
 
-    // Constructors, getters, setters, and other methods
+    public Customer(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
