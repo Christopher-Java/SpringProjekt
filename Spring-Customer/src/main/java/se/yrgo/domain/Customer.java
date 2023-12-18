08271b966ce8dc951c383ef19368593120d7f070
LinusPihl <123949708+LinusPihl@users.noreply.github.com>
2023-12-18 10:21:14 +0100

    Add files via upload

--- Spring-Customer/src/main/java/se/yrgo/domain/Customer.java
+++ Spring-Customer/src/main/java/se/yrgo/domain/Customer.java
@@ -1,4 +1,18 @@
+// Customer.java
 package se.yrgo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Constructors, getters, setters, and other methods
 }
--- Spring-Customer/src/main/java/se/yrgo/service/CustomerService.java
+++ Spring-Customer/src/main/java/se/yrgo/service/CustomerService.java
@@ -1,4 +1,44 @@
+// CustomerService.java
 package se.yrgo.service;
 
-public interface CustomerService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Long customerId) {
+        return customerRepository.findById(customerId).orElse(null);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Long customerId, Customer updatedCustomer) {
+        return customerRepository.findById(customerId)
+                .map(existingCustomer -> {
+                    existingCustomer.setName(updatedCustomer.getName());
+                    existingCustomer.setEmail(updatedCustomer.getEmail());
+                    return customerRepository.save(existingCustomer);
+                })
+                .orElse(null);
+    }
+
+    public void deleteCustomer(Long customerId) {
+        customerRepository.deleteById(customerId);
+    }
 }
